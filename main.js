9b728b7e25821d25401399d0cd47db7b36dd3b3a
HUZAIFA SHIEKH <147972420+Huzzi123s@users.noreply.github.com>
2024-04-04 00:02:27 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+#! usr/bin/env node
+import inquirer from "inquirer";
+let answer = await inquirer.prompt([
+    {
+        name: "firstname",
+        type: "number",
+        message: "Enter first Number",
+    },
+    {
+        name: "secondname",
+        type: "number",
+        message: "Enter second Number",
+    },
+    {
+        name: "operator",
+        type: "list",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+        message: "Enter first Number",
+    }
+]);
+if (answer.operator === "Addition") {
+    console.log(answer.firstname + answer.secondname);
+}
+else if (answer.operator === "Subtraction") {
+    console.log(answer.firstname - answer.secondname);
+}
+else if (answer.operator === "Multiplication") {
+    console.log(answer.firstname * answer.secondname);
+}
+else if (answer.operator === "Division") {
+    console.log(answer.firstname / answer.secondname);
+}
+else {
+    console.log("invalid value");
+}
